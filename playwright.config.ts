3c2eea50d47ad0c28f70e27d0b98a90df17de2e1
Jiří Zuna <j.zuna@seznam.cz>
2025-11-06 16:22:57 +0100

    Disable retries and enable parallel execution in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,9 +18,9 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 4,
+  retries: 0,
+  /* Run tests in parallel both locally and in CI */
+  workers: 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html"], ["line"]],
   globalTimeout:
